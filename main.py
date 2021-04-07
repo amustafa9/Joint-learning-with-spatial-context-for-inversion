d7f2b1b6c5cce0bac50dc1f6b0135ef7437475c5
amustafa9 <amustafa9@gatech.edu>
2021-04-07 19:38:14 -0400

    running from command line

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from utils import extract, standardize
 from datasets import SeismicDataset
 from torch.utils.data import DataLoader
 from model import Model
+from sklearn.metrics import r2_score
+import errno
+import argparse
 
     
 def preprocess(no_wells_marmousi, no_wells_seam):
@@ -68,7 +71,8 @@ def train(**kwargs):
     """Function trains 2-D TCN as specified in the paper"""
     
     # obtain data
-    seismic_marmousi, seismic_seam, model_marmousi, model_seam = preprocess(50, 12)
+    seismic_marmousi, seismic_seam, model_marmousi, model_seam = preprocess(kwargs['no_wells_marmousi'],\
+                                                                            kwargs['no_wells_seam'])
     
     # specify width of seismic image samples around each pseudolog
     width = 7
@@ -98,7 +102,7 @@ def train(**kwargs):
     model_seam = Model(1,1,[10, 30, 60, 90, 120], 9, 0.4).to(device)
     
     # define weight sharing value
-    gamma = torch.tensor([1e-4], requires_grad=True, dtype=torch.float, device=device)  # learnable weight for weight mismatch loss
+    gamma = torch.tensor([kwargs['gamma']], requires_grad=True, dtype=torch.float, device=device)  # learnable weight for weight mismatch loss
     
     # Set up loss
     criterion = torch.nn.MSELoss()
@@ -113,7 +117,7 @@ def train(**kwargs):
                                      lr=0.001)
     
     # start training 
-    for epoch in range(900):
+    for epoch in range(kwargs['epochs']):
     
       loss1 = torch.tensor([0.0], requires_grad=True).float().cuda()
       model_marmousi.train()
@@ -146,10 +150,142 @@ def train(**kwargs):
       optimizer_marmousi.step()
       optimizer_seam.step()
       
-      print('Epoch: {} | Marmousi Loss: {:0.4f} | Seam Loss: {:0.4f} | Val Loss: {:0.4f} | Mismatch Loss: {:0.4f} | Gamma: {:0.4f}'.format(epoch, loss1.item(), loss2.item(), val_loss.item(), weight_mismatch_loss.item(), gamma.item()))  
+      print('Epoch: {} | Marmousi Loss: {:0.4f} | Seam Loss: {:0.4f} | Val Loss: {:0.4f} | Mismatch Loss: {:0.4f} | Gamma: {:0.4f}\
+            '.format(epoch, loss1.item(), loss2.item(), val_loss.item(), \
+            weight_mismatch_loss.item(), gamma.item()))  
+    
+    # save trained models
+    if not os.path.isdir('saved_models'):  # check if directory for saved models exists
+        os.mkdir('saved_models')
         
+    torch.save(model_seam.state_dict(), 'saved_models/model_seam.pth')
+    torch.save(model_marmousi.state_dict(), 'saved_models/model_marmousi.pth')
 
+def test(**kwargs):
+    """Function tests the trained network on SEAM and Marmousi sections and 
+    prints out the results"""
+    
+    # obtain data
+    seismic_marmousi, seismic_seam, model_marmousi, model_seam = preprocess(kwargs['no_wells_marmousi'],\
+                                                                            kwargs['no_wells_seam'])
+    
+    # define device for training
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # specify width of seismic image samples around each pseudolog
+    width = 7
+    offset = int(width/2)
+    
+    # specify pseudolog positions for testing 
+    traces_marmousi_test = np.linspace(451, 2199, 2199-451+1, dtype=int)
+    traces_seam_test = np.linspace(offset, len(model_seam)-offset-1, len(model_seam)-int(2*offset), dtype=int)
+    
+    marmousi_test_dataset = SeismicDataset(seismic_marmousi, model_marmousi, traces_marmousi_test, width)
+    marmousi_test_loader = DataLoader(marmousi_test_dataset, batch_size = 16)
+    
+    seam_test_dataset = SeismicDataset(seismic_seam, model_seam, traces_seam_test, width)
+    seam_test_loader = DataLoader(seam_test_dataset, batch_size = 8)
+    
+    # load saved models
+    if not os.path.isdir('saved_models'):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), 'saved_models')
+        
+    # set up models
+    model_marmousi = Model(1,1,[10, 30, 60, 90, 120], 9, 0.4).to(device)
+    model_seam = Model(1,1,[10, 30, 60, 90, 120], 9, 0.4).to(device)
+
+    model_seam.load_state_dict(torch.load('saved_models/model_seam.pth'))
+    model_marmousi.load_state_dict(torch.load('saved_models/model_marmousi.pth'))
+    
+    # infer on SEAM
+    print("Inferring on SEAM...")
+    x, y = seam_test_dataset[0]  # get a sample
+    AI_pred = torch.zeros((len(seam_test_dataset), y.shape[-1])).float().to(device)
+    AI_act = torch.zeros((len(seam_test_dataset), y.shape[-1])).float().to(device)
+    
+    mem = 0
+    with torch.no_grad():
+        for i, (x,y) in enumerate(seam_test_loader):
+          model_seam.eval()
+          y_pred, _ = model_seam(x)
+          AI_pred[mem:mem+len(x)] = y_pred.squeeze().data
+          AI_act[mem:mem+len(x)] = y.squeeze().data
+          mem += len(x)
+          del x, y, y_pred
+    
+    vmin, vmax = AI_act.min(), AI_act.max()
+
+    AI_pred = AI_pred.detach().cpu().numpy()
+    AI_act = AI_act.detach().cpu().numpy()
+    print('r^2 score: {:0.4f}'.format(r2_score(AI_act.T, AI_pred.T)))
+    print('MSE: {:0.4f}'.format(np.sum((AI_pred-AI_act).ravel()**2)/AI_pred.size))
+    print('MAE: {:0.4f}'.format(np.sum(np.abs(AI_pred - AI_act)/AI_pred.size)))
+    print('MedAE: {:0.4f}'.format(np.median(np.abs(AI_pred - AI_act))))
+    
+    fig, (ax1, ax2) = plt.subplots(2,1, figsize=(12,12))
+    ax1.imshow(AI_pred.T, vmin=vmin, vmax=vmax, extent=(0,35000,15000,0))
+    ax1.set_aspect(35/30)
+    ax1.set_xlabel('Distance Eastimg (m)')
+    ax1.set_ylabel('Depth (m)')
+    ax1.set_title('Predicted')
+    ax2.imshow(AI_act.T, vmin=vmin, vmax=vmax, extent=(0,35000,15000,0))
+    ax2.set_aspect(35/30)
+    ax2.set_xlabel('Distance Eastimg (m)')
+    ax2.set_ylabel('Depth (m)')
+    ax2.set_title('Ground-Truth')
+    plt.show()
 
     
-train(no_wells_marmousi=50, no_wells_seam=12)
+    # infer on marmousi 2
+    print('\nInferring on Marmousi...')
+    x, y = marmousi_test_dataset[0]  # get a sample
+    AI_pred = torch.zeros((len(marmousi_test_dataset), y.shape[-1])).float().to(device)
+    AI_act = torch.zeros((len(marmousi_test_dataset), y.shape[-1])).float().to(device)
     
+    mem = 0
+    with torch.no_grad():
+        for i, (x,y) in enumerate(marmousi_test_loader):
+          model_marmousi.eval()
+          y_pred, _ = model_marmousi(x)
+          AI_pred[mem:mem+len(x)] = y_pred.squeeze().data
+          AI_act[mem:mem+len(x)] = y.squeeze().data
+          mem += len(x)
+          del x, y, y_pred
+          
+        vmin, vmax = AI_act.min(), AI_act.max()
+
+    AI_pred = AI_pred.detach().cpu().numpy()
+    AI_act = AI_act.detach().cpu().numpy()
+    
+    print('r^2 score: {:0.4f}'.format(r2_score(AI_act.T, AI_pred.T)))
+    print('MSE: {:0.4f}'.format(np.sum((AI_pred-AI_act).ravel()**2)/AI_pred.size))
+    print('MAE: {:0.4f}'.format(np.sum(np.abs(AI_pred - AI_act)/AI_pred.size)))
+    print('MedAE: {:0.4f}'.format(np.median(np.abs(AI_pred - AI_act))))
+    
+    fig, (ax1, ax2) = plt.subplots(2,1, figsize=(12,12))
+    ax1.imshow(AI_pred.T, vmin=vmin, vmax=vmax)
+    ax1.set_title('Predicted')
+    ax2.imshow(AI_act.T, vmin=vmin, vmax=vmax)
+    ax2.set_title('Ground-truth')
+    plt.show()
+      
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Hyperparams')
+    
+    parser.add_argument('--epochs', nargs='?', type=int, default=900,
+                        help='Number of epochs. Default = 1000')
+    parser.add_argument('--no_wells_marmousi', nargs='?', type=int, default=50,
+                        help='Number of sampled pseudologs for marmousi. Default = 50.')
+    parser.add_argument('--no_wells_seam', nargs='?', type=int, default=12,
+                        help='Number of sampled pseudologs for marmousi. Default = 12.')
+    parser.add_argument('--gamma', nargs='?', type=float, default=1e-4,
+                        help='Gamma value for soft sharing loss. Default = 1e-4')
+
+    args = parser.parse_args()
+    
+    train(no_wells_marmousi=args.no_wells_marmousi, no_wells_seam=args.no_wells_seam, 
+          epochs=args.epochs, gamma=args.gamma)
+    
+    test(no_wells_marmousi=args.no_wells_marmousi, no_wells_seam=args.no_wells_seam)
